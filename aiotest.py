f160ca9a29c664ed06efa53a5cfd552f398bbd3c
kaguya <kaguya@qq.com>
2023-04-19 16:15:54 +0800

    文字测试第二版

--- aiotest.py
+++ aiotest.py
@@ -1,6 +1,7 @@
 import aiohttp
 import asyncio
 import os
+from urllib.parse import unquote
 from lxml import etree
 from PIL import Image
 from pathlib import Path
@@ -13,15 +14,40 @@ async def fetch(session, url):
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        # html = await fetch(session, 'https://www.masterduelmeta.com/top-decks/diamond-i/april-2023/tearlaments/brock/LsYbU')
-        # demo=etree.HTML(html)
+        html = await fetch(session, 'https://www.masterduelmeta.com/top-decks/diamond-i/april-2023/tearlaments/brock/LsYbU')
+        demo=etree.HTML(html)
+        div=demo.xpath('//div[@class="svelte-16h7w79"]')
+        for i in div:
+            amounts=i.xpath('./div/img/@alt')
+            amounts=("".join(amounts)).split(' copies')[0]
+            href=i.xpath('./a/@href')
+            href="".join(href)
+            url_encode=unquote(href,'utf-8')
+            url_spi=url_encode.split('/',2)[2]
+            await (en_to_cn(url_spi))
+            if(amounts==""):
+                print(url_spi+" ×1")
+            else:
+                print(url_spi+" ×"+amounts)
+        # href=demo.xpath('//a[@class="image-wrapper svelte-1xqpccf"]/@href')
+        # names=[]
+        # for i in href:
+        #     url_encode=unquote(i,'utf-8')
+        #     url_spi=url_encode.split('/',2)[2]
+        #     names.append(url_spi)
+        #     # print(url_spi)
+        # print(names)
+
+        # amouts=demo.xpath('//img[@class="card-amount svelte-1xqpccf"]/@alt')
+        # print(amouts)
+
         # imgs=demo.xpath('//img[@class="card-img full-width svelte-1xqpccf"]/@alt')
         # img_url=[]
         # for img in imgs:
         #     img_url.append("https://s3.duellinksmeta.com/cards/"+img+"_w200.webp")
         # for imgurl in img_url:
         #     print(await down_img(session, imgurl))
-        await webp_to_jpg()
+        # await webp_to_jpg()
         # for i in img_url:
             # print(i)
         # img_url=https://s3.duellinksmeta.com/cards/"name"_w100.webp
@@ -29,23 +55,33 @@ async def main():
         #     print()
         # t=etree.tostring(demo,encoding="utf-8",pretty_print=True)
         # print(html)
-async def down_img(session,url):
-    name=url.split('/')[-1]
-    img=await session.get(url)
-    content =await img.read()
-    with open('./picture/'+str(name),'wb')as f:
-        # 写入至文件
-        f.write(content)
-        print(f'{name} 下载完成！')
-        return str(url)
-async def webp_to_jpg():
-    file="./picture"
-    picture=[]
-    for root,dirs,files in os.walk(file):
-        for file in files:
-            path=os.path.join(root,file)
-            img=Image.open(path)
-            image=img.convert('RGB')
-            image.save("./picture/"+file.split(".")[0]+".jpg")
+# async def down_img(session,url):
+#     name=url.split('/')[-1]
+#     img=await session.get(url)
+#     content =await img.read()
+#     with open('./picture/'+str(name),'wb')as f:
+#         # 写入至文件
+#         f.write(content)
+#         print(f'{name} 下载完成！')
+#         return str(url)
+# async def webp_to_jpg():
+#     file="./picture"
+#     picture=[]
+#     for root,dirs,files in os.walk(file):
+#         for file in files:
+#             path=os.path.join(root,file)
+#             img=Image.open(path)
+#             image=img.convert('RGB')
+#             image.save("./picture/"+file.split(".")[0]+".jpg")
 
+async def en_to_cn(key:str):
+    url=f"https://ygocdb.com/?search={key}"
+    print(key)
+    async with aiohttp.ClientSession()as session:
+        c= await fetch(session, url)
+        demo=etree.HTML(c)
+        # print(demo)
+        cnName=demo.xpath("/html/body/main/div/div[2]/div[2]/h2/span/text()")
+        print(cnName)
+        # return cnName
 asyncio.run(main())
--- src/plugins/ygodecksearch/plugins/__init__.py
+++ src/plugins/ygodecksearch/plugins/__init__.py
@@ -1,39 +1,70 @@
 import aiohttp
 import nonebot.plugin
 import nonebot
+from urllib.parse import unquote
 from nonebot.adapters.onebot.v11 import Bot,Message,MessageSegment,PrivateMessageEvent,GroupMessageEvent,GROUP_ADMIN,GROUP_OWNER,Event
 from nonebot.plugin import on_regex
 from nonebot.plugin import on_keyword
 from lxml import etree
 
-deck=on_keyword(['/deck'])
+deck=on_keyword(['/deck','卡组'])
 @deck.handle()
 async def deckMethod(bot: Bot,event: Event):
-    print("decktest")
+    ####img#####
     # key =str(event.message).strip()[3:].strip()
-    imgs=(await main())
-    msg=None
-    print(imgs)
-    for data in imgs:
-        msg+=MessageSegment.image(data)
+    # imgs=(await main())
+    # msg=None
+    # print(imgs)
+    # for data in imgs:
+    #     msg+=MessageSegment.image(data)
+    # if isinstance(event,GroupMessageEvent):
+    #     await send_forward_msg_group(bot,event,"阴影",msg if msg else ["空"])
+    # elif isinstance(event,PrivateMessageEvent):
+    #     await bot.send(event=event,message =msg if msg else "空")
+
+####msg####
+    msg=(await main())
+    # print(msg)
     if isinstance(event,GroupMessageEvent):
-        await send_forward_msg_group(bot,event,"阴影",msg if msg else ["空"])
+        await bot.send(event=event, message=msg if msg else "空")
+        # await send_forward_msg_group(bot,event,"阴影",msg if msg else ["空"])
     elif isinstance(event,PrivateMessageEvent):
         await bot.send(event=event,message =msg if msg else "空")
-
 async def fetch(session, url):
     async with session.get(url) as response:
         return await response.text()
 
 async def main():
+    ##图片版##
+    # async with aiohttp.ClientSession() as session:
+    #     html = await fetch(session,'https://www.masterduelmeta.com/top-decks/diamond-i/april-2023/tearlaments/brock/LsYbU')
+    #     demo = etree.HTML(html)
+    #     imgs = demo.xpath('//img[@class="card-img full-width svelte-1xqpccf"]/@alt')
+    #     img_url = []
+    #     for img in imgs:
+    #         img_url.append("https://s3.duellinksmeta.com/cards/" + img + "_w100.webp")
+    #     return img_url
+
+    ##文字版##
     async with aiohttp.ClientSession() as session:
         html = await fetch(session,'https://www.masterduelmeta.com/top-decks/diamond-i/april-2023/tearlaments/brock/LsYbU')
         demo = etree.HTML(html)
-        imgs = demo.xpath('//img[@class="card-img full-width svelte-1xqpccf"]/@alt')
-        img_url = []
-        for img in imgs:
-            img_url.append("https://s3.duellinksmeta.com/cards/" + img + "_w200.webp")
-        return img_url
+        div = demo.xpath('//div[@class="svelte-16h7w79"]')
+        msg = ""
+        for i in div:
+            amounts = i.xpath('./div/img/@alt')
+            amounts = ("".join(amounts)).split(' copies')[0]
+            href = i.xpath('./a/@href')
+            href = "".join(href)
+            url_encode = unquote(href, 'utf-8')
+            url_spi = url_encode.split('/', 2)[2]
+            name=await (en_to_cn(url_spi))
+            if (amounts == ""):
+                msg = msg + name + " ×1\n"
+            else:
+                msg = msg + name + " ×" + amounts + "\n"
+        # print(msg)
+        return msg
 
 
 # 合并消息
@@ -63,3 +94,10 @@ async def send_forward_msg_person(
     await bot.call_api(
         "send_private_forward_msg", user_id=event.user_id, messages=messages
     )
+async def en_to_cn(key:str):
+    url=f"https://ygocdb.com/?search={key}"
+    async with aiohttp.ClientSession()as session:
+        c=await fetch(session, url)
+        demo=etree.HTML(c)
+        cnName=demo.xpath("/html/body/main/div/div[2]/div[2]/h2/span/text()")
+        return "".join(cnName)
